d7b23067813d3fedcb4d75189babc482aa35d269
JADEILTON <jadeiltondm@gmail.com>
2023-08-15 14:25:39 -0300

    15/08/2023

--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -12,7 +12,23 @@ import java.util.List;
 
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
     Page<Medico> findAllByAtivoTrue(Pageable paginacao);
-    @Query()
+    @Query("""
+            select m from Medico m
+            where
+            m.ativo
+            and
+            m.especialidade = :especialidade
+            and
+            m.id not in(
+                select c.medico.id from Consulta c
+                where
+                c.dataTime = :dataTime
+            and
+                c.motivoCancelamento is null
+            )
+            order by rand()
+            limit 1
+""")
     Medico escolherMedicoAleatorioLivreNaData(Especialidade especialidade, LocalDateTime data);
     @Query("""
             select m.ativo
