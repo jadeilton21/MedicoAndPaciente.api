2876c8279e25bce1501e8d33d920b276763f1317
JADEILTON <jadeiltondm@gmail.com>
2023-09-28 16:26:41 -0300

    28/09/2023

--- src/test/java/med/voll/api/controller/MedicoControllerTest.java
+++ src/test/java/med/voll/api/controller/MedicoControllerTest.java
@@ -1,9 +1,9 @@
 package med.voll.api.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import med.voll.api.domain.medico.DadosCadastroMedico;
-import med.voll.api.domain.medico.DadosDetalhamentoMedico;
-import med.voll.api.domain.medico.MedicoRepository;
+import med.voll.api.domain.endereco.DadosEndereco;
+import med.voll.api.domain.endereco.Endereco;
+import med.voll.api.domain.medico.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
@@ -20,6 +21,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest(classes = MedicoController.class )
@@ -62,40 +65,49 @@ class MedicoControllerTest {
     @DisplayName("Deve devolver código http 200 quando informações estão validas")
     @WithMockUser
     void cadastrar_cenario2() throws Exception{
+        var dadosCadastro = new DadosCadastroMedico(
+                "Medico",
+                "medico@voll.med",
+                "61999999999",
+                "123456",
+                Especialidade.CARDIOLOGIA,
+                dadosEndereco());
+
+        when(medicoRepository.save(any())).thenReturn(new Medico(dadosCadastro));
+
+        var response = mockMvc
+                .perform(post("/medicos")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(dadosCadastroMedicoJson.write(dadosCadastro).getJson()))
+                .andReturn().getResponse();
+
+        var dadosDetalhamento = new DadosDetalhamentoMedico(
+                null,
+                dadosCadastro.nome(),
+                dadosCadastro.email(),
+                dadosCadastro.crm(),
+                dadosCadastro.telefone(),
+                dadosCadastro.especialidade(),
+                new Endereco(dadosCadastro.endereco())
+        );
+        var jsonEsperado = dadosDetalhamentoMedicoJson.write(dadosDetalhamento).getJson();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(response.getContentAsString()).isEqualTo(jsonEsperado);
 
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    private DadosEndereco dadosEndereco() {
+        return new DadosEndereco(
+                "rua xpto",
+                "bairro",
+                "00000000",
+                "Brasilia",
+                "DF",
+                null,
+                null
+        );
+    }
 
 
 }
