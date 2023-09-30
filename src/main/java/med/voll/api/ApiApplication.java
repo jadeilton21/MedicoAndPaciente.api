3b81d86724150e42cf1de08e6e190c39bbde4a86
JADEILTON <jadeiltondm@gmail.com>
2023-09-30 10:09:56 -0300

    30/09/2023

--- src/main/java/med/voll/api/ApiApplication.java
+++ src/main/java/med/voll/api/ApiApplication.java
@@ -1,13 +1,20 @@
 package med.voll.api;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class ApiApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+
+
+
 	}
 
 }
