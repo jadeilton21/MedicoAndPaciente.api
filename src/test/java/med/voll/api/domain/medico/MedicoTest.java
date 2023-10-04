9bd8e4a61f82139c4b1ee383cbce5020f1ed8ac1
JADEILTON <jadeiltondm@gmail.com>
2023-10-04 14:31:42 -0300

    04/10/2023, testando o metodo atualizarInformacoes da classe Medico.

---
+++ src/test/java/med/voll/api/domain/medico/MedicoTest.java
@@ -0,0 +1,12 @@
+package med.voll.api.domain.medico;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MedicoTest {
+
+    @Test
+    void atualizarInformacoes() {
+    }
+}
