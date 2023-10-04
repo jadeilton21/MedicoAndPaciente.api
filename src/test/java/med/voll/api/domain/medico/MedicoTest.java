5db2b9c451a7a67c974af418c9dbd46fa38c3b07
JADEILTON <jadeiltondm@gmail.com>
2023-10-04 14:34:21 -0300

    04/10/2023, testando o metodo atualizarInformacoes da classe Medico.

--- src/test/java/med/voll/api/domain/medico/MedicoTest.java
+++ src/test/java/med/voll/api/domain/medico/MedicoTest.java
@@ -1,12 +1,21 @@
 package med.voll.api.domain.medico;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
 class MedicoTest {
 
     @Test
-    void atualizarInformacoes() {
+    @DisplayName("Deve Devolver Código Http 400 quando informações forem invalidas.")
+    @WithMockUser
+    void atualizarInformacoes_cenario1() throws Exception{
     }
 }
