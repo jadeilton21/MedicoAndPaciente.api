3e998133f786815498b28611cbdda2b6f5a66192
JADEILTON <jadeiltondm@gmail.com>
2023-10-05 07:31:18 -0300

    05/10/2023, testando o metodo atualizarInformacoes da classe Medico.

--- src/test/java/med/voll/api/domain/medico/MedicoTest.java
+++ src/test/java/med/voll/api/domain/medico/MedicoTest.java
@@ -2,20 +2,36 @@ package med.voll.api.domain.medico;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureJsonTesters
 class MedicoTest {
 
+
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
     @DisplayName("Deve Devolver Código Http 400 quando informações forem invalidas.")
     @WithMockUser
     void atualizarInformacoes_cenario1() throws Exception{
+
+        var response = mockMvc
+                .perform(post("/medicos"))
+                .andReturn().getResponse();
+        assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 }
