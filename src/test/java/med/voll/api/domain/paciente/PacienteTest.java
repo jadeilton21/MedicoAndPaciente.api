88bac68cf9bcc97b2d30edeab17a482a24928edd
JADEILTON <jadeiltondm@gmail.com>
2023-10-07 08:38:59 -0300

    07/10/2023, testando o metodo atualizarInformacoes da classe Paciente.

--- src/test/java/med/voll/api/domain/paciente/PacienteTest.java
+++ src/test/java/med/voll/api/domain/paciente/PacienteTest.java
@@ -1,12 +1,37 @@
 package med.voll.api.domain.paciente;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
 class PacienteTest {
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
-    void atualizarInformacoes() {
+    @DisplayName("Deve Devolver Códgio Http 400 quando informações estiverem Invalidas..")
+    @WithMockUser
+    void atualizarInformacoes() throws Exception {
+
+        var response = mockMvc
+                .perform(post("/pacientes"))
+                .andReturn().getResponse();
+
+        assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 }
