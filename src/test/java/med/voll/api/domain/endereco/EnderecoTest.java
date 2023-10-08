0581a0cb2d356ca40b94c305a1a4edabc53c3fa2
JADEILTON <jadeiltondm@gmail.com>
2023-10-08 11:46:00 -0300

    08/10/2023, testando o metodo atualizarInformacoes da classe Endereco.

--- src/test/java/med/voll/api/domain/endereco/EnderecoTest.java
+++ src/test/java/med/voll/api/domain/endereco/EnderecoTest.java
@@ -1,9 +1,14 @@
 package med.voll.api.domain.endereco;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
 class EnderecoTest {
 
     @Test
