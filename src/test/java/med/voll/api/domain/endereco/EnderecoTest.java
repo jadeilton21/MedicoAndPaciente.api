d7a01e79e82935e59de064ee2512ec824cf41bc9
JADEILTON <jadeiltondm@gmail.com>
2023-10-08 11:43:31 -0300

    08/10/2023, testando o metodo atualizarInformacoes da classe Endereco.

---
+++ src/test/java/med/voll/api/domain/endereco/EnderecoTest.java
@@ -0,0 +1,12 @@
+package med.voll.api.domain.endereco;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EnderecoTest {
+
+    @Test
+    void atualizarInformacoes() {
+    }
+}
