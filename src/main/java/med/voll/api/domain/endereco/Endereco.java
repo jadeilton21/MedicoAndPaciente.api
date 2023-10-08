6a4193f9763c71c4ab0710f912122e90ee7f050f
JADEILTON <jadeiltondm@gmail.com>
2023-10-08 20:17:37 -0300

    08/10/2023, Build do projeto

--- src/main/java/med/voll/api/domain/endereco/Endereco.java
+++ src/main/java/med/voll/api/domain/endereco/Endereco.java
@@ -1,6 +1,8 @@
 package med.voll.api.domain.endereco;
 
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
