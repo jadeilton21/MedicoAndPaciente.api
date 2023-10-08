16ec35e486bce8814f735036775b8a8ba6bd4829
JADEILTON <jadeiltondm@gmail.com>
2023-10-08 11:49:07 -0300

    08/10/2023, testando o metodo atualizarInformacoes da classe Endereco.

--- src/test/java/med/voll/api/domain/endereco/EnderecoTest.java
+++ src/test/java/med/voll/api/domain/endereco/EnderecoTest.java
@@ -1,9 +1,13 @@
 package med.voll.api.domain.endereco;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
@@ -11,7 +15,13 @@ import static org.junit.jupiter.api.Assertions.*;
 @AutoConfigureJsonTesters
 class EnderecoTest {
 
+
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
+    @DisplayName("Deve Devolver Cógido http 400 quando informações estiverem Invalidas..")
+    @WithMockUser
     void atualizarInformacoes() {
     }
 }
