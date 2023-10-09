5ae3ea654e7226a61d16cc51a7c12e6804bcff9a
JADEILTON <jadeiltondm@gmail.com>
2023-10-09 14:19:55 -0300

    09/10/2023, testando a classe PacienteRepository.

--- src/test/java/med/voll/api/domain/paciente/PacienteRepositoryTest.java
+++ src/test/java/med/voll/api/domain/paciente/PacienteRepositoryTest.java
@@ -1,7 +1,9 @@
 package med.voll.api.domain.paciente;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,5 +11,12 @@ import static org.junit.jupiter.api.Assertions.*;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
 class PacienteRepositoryTest {
+
+    @Autowired
+    private PacienteRepository repository;
+    @Autowired
+    private TestEntityManager em;
+
+
 
 }
